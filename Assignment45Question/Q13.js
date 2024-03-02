9d77d5f0339a93fb6e968b640d18003b7b0e2f38
Tanzeel Sarwar <160385969+TanzeelSarwarMughal@users.noreply.github.com>
2024-03-02 13:55:06 +0500

    Delete Assignment45Question directory

--- Assignment45Question/Q13.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
-// Define an array of my favorite cars
-let cars = ["Supra Mk5", "Supra Mk4", "Honda Civic Type R", "Mercedes Benz"];
-// Use a for loop to iterate over the array and print a statement about each car
-for (let i = 0; i < cars.length; i++) {
-    console.log(`I would like to own a ${cars[i]}.`);
-}
